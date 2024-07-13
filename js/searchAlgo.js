99d1997b88a7437e6d3e1b354b4bacf03fd764f1
yugamsaini <yugamsaini4april@gmail.com>
2024-07-13 14:59:31 +0530

    greedy algo

--- js/searchAlgo.js
+++ js/searchAlgo.js
@@ -121,3 +121,114 @@ function Dijkstra() {
         }
     }
 }
+// ======================= Astar ⚙️🦾 ======================
+// ==========================================================
+
+function Astar() {
+    const queue = new PriorityQueue();;
+    const visited = new Set();//closedset
+    const queued = new Set();//openset
+    const parent = new Map();
+    const gScore = [];
+
+    for (let i = 0; i < row; i++) {
+        const INF = [];
+        for (let j = 0; j < col; j++) {
+            INF.push(Infinity);
+        }
+        gScore.push(INF);
+    }
+
+    gScore[source.x][source.y] = 0;
+    queue.push({ cordinate: source, cost: heuristicValue(source) });
+    visited.add(`${source.x}-${source.y}`);
+
+    while (queue.length > 0) {
+        const { cordinate: current } = queue.pop();
+        searchToAnimate.push(matrix[current.x][current.y]);
+
+        //you find the target
+        if (current.x === target.x && current.y === target.y) {
+            pathToAnimate = backtrack(parent, target).reverse();
+            return;
+        }
+
+        visited.add(`${current.x}-${current.y}`);
+
+        const neighbours = getNeighbours(current);
+
+        for (const neighbour of neighbours) {
+            const key = `${neighbour.x}-${neighbour.y}`;
+
+            if (isValid(neighbour.x, neighbour.y) &&
+                !visited.has(key) &&
+                !queued.has(key) &&
+                !matrix[neighbour.x][neighbour.y].classList.contains('wall')
+            ) {
+
+                //Assuming edge weight = 1, between adjacent vertices
+                const edgeWeight = 1;
+                const gScoreToNeighbour = gScore[current.x][current.y] + edgeWeight;
+                const fScore = gScoreToNeighbour + heuristicValue(neighbour);
+
+                if (gScoreToNeighbour < gScore[neighbour.x][neighbour.y]) {
+                    gScore[neighbour.x][neighbour.y] = gScoreToNeighbour;
+
+                    queue.push({ cordinate: neighbour, cost: fScore });
+                    queued.add(key);//openset
+
+                    parent.set(key, current);
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+// ==========================================================
+// ====================== Greedy ⚙️🦾 ======================
+// ==========================================================
+
+function greedy() {
+    const queue = new PriorityQueue();
+    const visited = new Set();
+    const parent = new Map();
+
+    queue.push({ cordinate: source, cost: heuristicValue(source) });
+    visited.add(`${source.x}-${source.y}`);
+
+    while (queue.length > 0) {
+        const { cordinate: current } = queue.pop();
+        searchToAnimate.push(matrix[current.x][current.y]);
+
+        //you find the target
+        if (current.x === target.x && current.y === target.y) {
+            pathToAnimate = backtrack(parent, target).reverse();
+            return;
+        }
+
+        const neighbours = [
+            { x: current.x - 1, y: current.y },//up
+            { x: current.x, y: current.y + 1 },//right
+            { x: current.x + 1, y: current.y },//bottom
+            { x: current.x, y: current.y - 1 }//right
+        ];
+
+        for (const neighbour of neighbours) {
+            const key = `${neighbour.x}-${neighbour.y}`;
+
+            if (isValid(neighbour.x, neighbour.y) &&
+                !visited.has(key) &&
+                !matrix[neighbour.x][neighbour.y].classList.contains('wall')
+            ) {
+                queue.push({ cordinate: neighbour, cost: heuristicValue(neighbour) });
+                visited.add(key);
+                parent.set(key, current);
+            }
+        }
+    }
+}
+
