ac8f71c29cf5421848f6f0fd2c17d1b2fa01fcb8
yugamsaini <yugamsaini4april@gmail.com>
2024-07-13 14:48:31 +0530

    updated javascript for baord

--- js/board.js
+++ js/board.js
@@ -53,3 +53,79 @@ function renderBoard() {
     boardInteraction(cells);
 }
 
+//  BOARD INTERATION 
+function boardInteraction(cells) {
+    let draging = false;
+    let drawing = false;
+    let dragStart = null;
+    cells.forEach((cell) => {
+        const pointDown = (e) => {
+            if (e.target.classList.contains('source')) {
+                dragStart = 'source';
+                draging = true;
+            }
+            else if (e.target.classList.contains('target')) {
+                dragStart = 'target';
+                draging = true;
+            }
+            else {
+                drawing = true;
+            }
+        }
+
+        const pointUp = () => {
+            drawing = false;
+            draging = false;
+            dragStart = null;
+            matrix[source.x][source.y].classList.remove('wall');
+            matrix[target.x][target.y].classList.remove('wall');
+        }
+
+        const pointMove = (e) => {
+            const triggerElement = document.elementFromPoint(e.clientX, e.clientY);
+            if (triggerElement == null || !triggerElement.classList.contains('col')) return;
+            cordinate = { ...triggerElement.id.split('-') };
+
+            if (draging && dragStart) {
+
+                cells.forEach(cell => {
+                    cell.classList.remove(dragStart);
+                })
+                triggerElement.classList.add(dragStart);
+
+                if (dragStart === 'source') {
+                    source.x = Number(cordinate[0]);
+                    source.y = Number(cordinate[1]);
+                }
+                else {
+                    target.x = Number(cordinate[0]);
+                    target.y = Number(cordinate[1]);
+                }
+            }
+
+
+            else if (drawing) {
+                if (triggerElement.classList.contains('source') || triggerElement.classList.contains('target'))
+                    return;
+
+                const x = Number(cordinate[0]);
+                const y = Number(cordinate[1]);
+
+                matrix[x][y].setAttribute('class', 'col wall');
+            }
+        }
+
+        cell.addEventListener('pointerdown', pointDown);
+        cell.addEventListener('pointermove', pointMove);
+        cell.addEventListener('pointerup', pointUp);
+
+        cell.addEventListener('click', () => {
+            if (cell.classList.contains('source') || cell.classList.contains('target'))
+                return;
+
+            cell.classList.remove('visited', 'path');
+            cell.classList.toggle('wall');
+        })
+    })
+
+}
